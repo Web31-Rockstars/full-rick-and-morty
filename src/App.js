31ae3e3345ec74ce8a5b19e5f7c735c3d21e279c
jasonelliots <j.elliotschwartz@gmail.com>
2020-05-18 17:03:15 -0700

    added axios and connect to rick and morty API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
-import './App.css';
+import React, {useEffect} from 'react';
+import './App.css'; 
+import axios from 'axios'
 
 function App() {
+
+useEffect(()=> {
+  axios.get('https://rickandmortyapi.com/api/character/')
+  .then(res => {
+    console.log(res.data)
+  })
+  .catch(err => {
+    console.log(err)
+  })
+}, []
+)
+  
   return (
     <div className="App">
       <header className="App-header">
